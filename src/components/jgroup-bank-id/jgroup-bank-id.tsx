196e8196cf30d3e03ddc8c1109060c19e7133c89
Johannes Eriksson <johannes.eriksson@hyperisland.se>
2024-11-27 12:57:28 +0100

    fix: reset history state when component loads

--- src/components/jgroup-bank-id/jgroup-bank-id.tsx
+++ src/components/jgroup-bank-id/jgroup-bank-id.tsx
@@ -171,6 +171,8 @@ export class JgroupBankId {
   componentWillLoad() {
     this.validateProps();
 
+    window.history.replaceState({}, null);
+
     this.init = this.init.bind(this);
     this.startOnAnotherDevice = this.startOnAnotherDevice.bind(this);
     this.pollCollect = this.pollCollect.bind(this);
